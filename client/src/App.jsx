246dc9f8aee3f9d31151e85c2e8eb4f7cc5c2970
Brent Cleary <brent.cleary@gmail.com>
2023-03-30 14:56:08 -0700

    Commit after conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { Dashboard } from "./views/Dashboard";
 import { CreateLog } from "./views/CreateLog";
 import { UpdateLog } from "./views/UpdateLog";
 import {LoginView} from "./views/LoginView"
+import jwt_decode from "jwt-decode"
 
 const theme = createTheme({
   palette: {
@@ -58,17 +59,69 @@ const theme = createTheme({
 });
 
 function App() {
-  const [currentUser, setCurrentUser] = useState({});
+
+  // User useState variable
+  const [user, setUser] = useState({});
+
+    // Initializes Client - Calls google OAuth - calls Sign-in Features
+    useEffect(() => {
+      /* global google */
+      google.accounts.id.initialize({
+        client_id: "377210060521-j5bmdlnnnkt8dvd6f7aqql61bd5qgo3k.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+      });
+  
+      // Button Render
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {theme: "outline", size: "large"}
+      );
+      
+      // loads Sign-In Prompt
+      google.accounts.id.prompt();
+  
+    }, []);
+
+  // Sets userState variable
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+    setUser(user);
+  }
+
+    // Sign-Out: Sets User to empty obj - unhides Sign-In Button 
+    function handleSignOut(event) {
+      setUser({});
+      document.getElementById("signInDiv").hidden = false;
+      setUser({})
+    }
 
   const loginUser = (user) => {
     setCurrentUser(user);
     console.log(currentUser);
   };
+
   return (
     <Box>
       
       <ThemeProvider theme={theme}>
         <MuiNavBar />
+        <div>
+          <div id="signInDiv"></div>
+          { Object.keys(user).length != 0 &&
+            <button onClick={ (e) => handleSignOut(e) }>Sign Out</button>
+          }
+
+          { user && 
+            <div>
+              <img src={user.picture}></img>
+              <h3>{user.name}</h3>
+            </div>
+          }
+        </div>
         <Routes>
           <Route path='/logs/login' element={<LoginView updateUser={loginUser} />}></Route>
           <Route path="/logs" element={<Dashboard />}></Route>
